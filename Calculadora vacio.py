da3c9546cb1230f3c19be33895cef982530c8662
unknown <brad.alvarez@epn.edu.ec>
2017-05-24 12:08:06 -0500

    ai creo q estan creado los botones mis shavos

--- Calculadora vacio.py
+++ Calculadora vacio.py
@@ -1,1 +1,16 @@
 from tkinter import *
+
+tk = Tk()
+btn1 = Button(tk, text = '1', temporal1 = 1)
+btn2 = Button(tk, text = '2', temporal2 = 2)
+btn3 = Button(tk, text = '3', temporal3 = 3)
+btn4 = Button(tk, text = '4', temporal4 = 4)
+btn5 = Button(tk, text = '5', temporal5 = 5)
+btn6 = Button(tk, text = '6', temporal6 = 6)
+btn7 = Button(tk, text = '7', temporal7 = 7)
+btn8 = Button(tk, text = '8', temporal8 = 8)
+btn9 = Button(tk, text = '9', temporal9 = 9)
+btn0 = Button(tk, text = '0', temporal0 = 0)
+
+
+
