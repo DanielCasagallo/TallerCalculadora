86dfa16d8a392603d2885f4249779bbbf315c1c7
22B15-PC\22B <daniel.casagallo@epn.edu.ec>
2017-05-24 12:41:45 -0500

    .

--- Calculadora vacio.py
+++ Calculadora vacio.py
@@ -1,16 +1,16 @@
 from tkinter import *
 
 tk = Tk()
-btn1 = Button(tk, text = '1', command = 1)
-btn2 = Button(tk, text = '2', command = 2)
-btn3 = Button(tk, text = '3', command = 3)
-btn4 = Button(tk, text = '4', command = 4)
-btn5 = Button(tk, text = '5', command = 5)
-btn6 = Button(tk, text = '6', command = 6)
-btn7 = Button(tk, text = '7', command = 7)
-btn8 = Button(tk, text = '8', command = 8)
-btn9 = Button(tk, text = '9', command = 9)
-btn0 = Button(tk, text = '0', command = 0)
+btn1 = Button(tk, text = '1', command=1)
+btn2 = Button(tk, text = '2', command=2)
+btn3 = Button(tk, text = '3', command=3)
+btn4 = Button(tk, text = '4', command=4)
+btn5 = Button(tk, text = '5', command=5)
+btn6 = Button(tk, text = '6', command=6)
+btn7 = Button(tk, text = '7', command=7)
+btn8 = Button(tk, text = '8', command=8)
+btn9 = Button(tk, text = '9', command=9)
+btn0 = Button(tk, text = '0', command=0)
 
 btn1.pack()
 btn2.pack()
@@ -23,7 +23,7 @@ btn8.pack()
 btn9.pack()
 btn0.pack()
 
-#123
+
 
 
 
