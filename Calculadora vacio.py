8d9bb0e1614968d42bf0ae82d49ea2d133237831
22B22-PC\22B <jona_maiza@hotmail.es>
2017-05-24 12:38:05 -0500

    s

--- Calculadora vacio.py
+++ Calculadora vacio.py
@@ -1,28 +1,40 @@
 from tkinter import *
 
 tk = Tk()
-btn1 = Button(tk, text = '1', command = 1)
-btn2 = Button(tk, text = '2', command = 2)
-btn3 = Button(tk, text = '3', command = 3)
-btn4 = Button(tk, text = '4', command = 4)
-btn5 = Button(tk, text = '5', command = 5)
-btn6 = Button(tk, text = '6', command = 6)
-btn7 = Button(tk, text = '7', command = 7)
-btn8 = Button(tk, text = '8', command = 8)
-btn9 = Button(tk, text = '9', command = 9)
-btn0 = Button(tk, text = '0', command = 0)
 
-btn1.pack()
-btn2.pack()
-btn3.pack()
-btn4.pack()
-btn5.pack()
-btn6.pack()
-btn7.pack()
-btn8.pack()
-btn9.pack()
-btn0.pack()
+def resta(x,y):
+    btn1 = Button(tk, text = 'Resta')
+   
+def botones():
+    global command
+    btn1 = Button(tk, text = '1', command = 1)
+    btn2 = Button(tk, text = '2', command = 2)
+    btn3 = Button(tk, text = '3', command = 3)
+    btn4 = Button(tk, text = '4', command = 4)
+    btn5 = Button(tk, text = '5', command = 5)
+    btn6 = Button(tk, text = '6', command = 6)
+    btn7 = Button(tk, text = '7', command = 7)
+    btn8 = Button(tk, text = '8', command = 8)
+    btn9 = Button(tk, text = '9', command = 9)
+    btn0 = Button(tk, text = '0', command = 0)
 
+    btn1.pack()
+    btn2.pack()
+    btn3.pack()
+    btn4.pack()
+    btn5.pack()
+    btn6.pack()
+    btn7.pack()
+    btn8.pack()
+    btn9.pack()
+    btn0.pack()
+    mainloop()
+    return command
+
+botones()
+x=int(botones())
+y=int(botones())
+print(resta(x,y))
 
 
 
