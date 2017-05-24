8d50bbfba99923b8dfe0c222851c14b1d38300ea
unknown <brad.alvarez@epn.edu.ec>
2017-05-24 12:17:25 -0500

    nose si este bien  :(

--- Calculadora vacio.py
+++ Calculadora vacio.py
@@ -1,16 +1,30 @@
 from tkinter import *
 
 tk = Tk()
-btn1 = Button(tk, text = '1', temporal1 = 1)
-btn2 = Button(tk, text = '2', temporal2 = 2)
-btn3 = Button(tk, text = '3', temporal3 = 3)
-btn4 = Button(tk, text = '4', temporal4 = 4)
-btn5 = Button(tk, text = '5', temporal5 = 5)
-btn6 = Button(tk, text = '6', temporal6 = 6)
-btn7 = Button(tk, text = '7', temporal7 = 7)
-btn8 = Button(tk, text = '8', temporal8 = 8)
-btn9 = Button(tk, text = '9', temporal9 = 9)
-btn0 = Button(tk, text = '0', temporal0 = 0)
+btn1 = Button(tk, text = '1', command = 1)
+btn2 = Button(tk, text = '2', command = 2)
+btn3 = Button(tk, text = '3', command = 3)
+btn4 = Button(tk, text = '4', command = 4)
+btn5 = Button(tk, text = '5', command = 5)
+btn6 = Button(tk, text = '6', command = 6)
+btn7 = Button(tk, text = '7', command = 7)
+btn8 = Button(tk, text = '8', command = 8)
+btn9 = Button(tk, text = '9', command = 9)
+btn0 = Button(tk, text = '0', command = 0)
+
+btn1.pack()
+btn2.pack()
+btn3.pack()
+btn4.pack()
+btn5.pack()
+btn6.pack()
+btn7.pack()
+btn8.pack()
+btn9.pack()
+btn0.pack()
+
+
+
 
 
 
