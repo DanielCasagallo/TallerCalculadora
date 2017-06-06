87fe883b26629438fb4bfecc3bc34489373ada72
DESKTOP-CAKR1N1\zero <luis191936@hotmail.com>
2017-06-05 20:01:00 -0500

    los botones

--- Calculadora vacio.py
+++ Calculadora vacio.py
@@ -1,67 +1,48 @@
-from tkinter import *
+from tkinter import*
+#mascara
+root=Tk()
+root.title("calculadora")
+operator=""
+text_Input=StringVar()
+txtDisplay=Entry(root,font=('arial',20,'bold'),
+                 textvariable=text_Input,
+                 bd=30,
+                 insertwidth=4,
+                 bg="powder blue",
+                 justify='right').grid(columnspan=4)
 
-tk = Tk()
-<<<<<<< HEAD
-
-def resta(x,y):
-    btn1 = Button(tk, text = 'Resta')
-   
-def botones():
-    global command
-    btn1 = Button(tk, text = '1', command = 1)
-    btn2 = Button(tk, text = '2', command = 2)
-    btn3 = Button(tk, text = '3', command = 3)
-    btn4 = Button(tk, text = '4', command = 4)
-    btn5 = Button(tk, text = '5', command = 5)
-    btn6 = Button(tk, text = '6', command = 6)
-    btn7 = Button(tk, text = '7', command = 7)
-    btn8 = Button(tk, text = '8', command = 8)
-    btn9 = Button(tk, text = '9', command = 9)
-    btn0 = Button(tk, text = '0', command = 0)
-
-    btn1.pack()
-    btn2.pack()
-    btn3.pack()
-    btn4.pack()
-    btn5.pack()
-    btn6.pack()
-    btn7.pack()
-    btn8.pack()
-    btn9.pack()
-    btn0.pack()
-    mainloop()
-    return command
-
-botones()
-x=int(botones())
-y=int(botones())
-print(resta(x,y))
-=======
-btn1 = Button(tk, text = '1', command=1)
-btn2 = Button(tk, text = '2', command=2)
-btn3 = Button(tk, text = '3', command=3)
-btn4 = Button(tk, text = '4', command=4)
-btn5 = Button(tk, text = '5', command=5)
-btn6 = Button(tk, text = '6', command=6)
-btn7 = Button(tk, text = '7', command=7)
-btn8 = Button(tk, text = '8', command=8)
-btn9 = Button(tk, text = '9', command=9)
-btn0 = Button(tk, text = '0', command=0)
-
-btn1.pack()
-btn2.pack()
-btn3.pack()
-btn4.pack()
-btn5.pack()
-btn6.pack()
-btn7.pack()
-btn8.pack()
-btn9.pack()
-btn0.pack()
-
->>>>>>> 86dfa16d8a392603d2885f4249779bbbf315c1c7
-
-
-
-
+btn1=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="1",bg="powder blue").grid(row=1,column=0)
+btn2=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="2",bg="powder blue").grid(row=1,column=1)
+btn3=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="3",bg="powder blue").grid(row=1,column=2)
+btn4=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="4",bg="powder blue").grid(row=2,column=0)
+btn5=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="5",bg="powder blue").grid(row=2,column=1)
+btn6=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="6",bg="powder blue").grid(row=2,column=2)
+btn7=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="7",bg="powder blue").grid(row=3,column=0)
+btn8=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="8",bg="powder blue").grid(row=3,column=1)
+btn9=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="9",bg="powder blue").grid(row=3,column=2)
+btn0=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="0",bg="powder blue").grid(row=4,column=0)
+#---------operaciones basicas-------------------------------------------
+btnmenos=Button(root,padx=18,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="-",bg="powder blue").grid(row=1,column=3)
+btnsuma=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',18,'bold'),text="+",bg="powder blue").grid(row=2,column=3)
+btnmultiplicacion=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',18,'bold'),text="*",bg="powder blue").grid(row=3,column=3)
+btndivision=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="/",bg="powder blue").grid(row=4,column=3)
+btnresultado=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="=",bg="powder blue").grid(row=4,column=2)
+btnborrar=Button(root,padx=16,bd=8,fg="black",font=
+            ('arial',20,'bold'),text="c",bg="powder blue").grid(row=4,column=1)
+root.mainloop()
 
