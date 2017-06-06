26bf874b325ad6bd63ff584b609a11b779b8dff9
HP\Daniel <edison.casagallo@epn.edu.ec>
2017-06-05 20:39:32 -0500

    Funcionalidad botones, creando la funcion Boton

--- Calculadora vacio.py
+++ Calculadora vacio.py
@@ -1,8 +1,15 @@
 from tkinter import*
+
+#funciones
+def Boton(numero):
+    global operador
+    operador = operador + str(numero)
+    text_Input.set(operador)
+    
 #mascara
 root=Tk()
 root.title("calculadora")
-operator=""
+operador=""
 text_Input=StringVar()
 txtDisplay=Entry(root,font=('arial',20,'bold'),
                  textvariable=text_Input,
@@ -11,26 +18,35 @@ txtDisplay=Entry(root,font=('arial',20,'bold'),
                  bg="powder blue",
                  justify='right').grid(columnspan=4)
 
-btn1=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="1",bg="powder blue").grid(row=1,column=0)
-btn2=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="2",bg="powder blue").grid(row=1,column=1)
-btn3=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="3",bg="powder blue").grid(row=1,column=2)
-btn4=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="4",bg="powder blue").grid(row=2,column=0)
-btn5=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="5",bg="powder blue").grid(row=2,column=1)
-btn6=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="6",bg="powder blue").grid(row=2,column=2)
-btn7=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="7",bg="powder blue").grid(row=3,column=0)
-btn8=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="8",bg="powder blue").grid(row=3,column=1)
-btn9=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="9",bg="powder blue").grid(row=3,column=2)
-btn0=Button(root,padx=16,bd=8,fg="black",font=
-            ('arial',20,'bold'),text="0",bg="powder blue").grid(row=4,column=0)
+btn1=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="1",
+            bg="powder blue",command=lambda:Boton(1)).grid(row=1,column=0)
+
+btn2=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="2",
+            bg="powder blue",command=lambda:Boton(2)).grid(row=1,column=1)
+
+btn3=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="3",
+            bg="powder blue",command=lambda:Boton(3)).grid(row=1,column=2)
+
+btn4=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="4",
+            bg="powder blue",command=lambda:Boton(4)).grid(row=2,column=0)
+
+btn5=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="5",
+            bg="powder blue",command=lambda:Boton(5)).grid(row=2,column=1)
+
+btn6=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="6",
+            bg="powder blue",command=lambda:Boton(6)).grid(row=2,column=2)
+
+btn7=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="7",
+            bg="powder blue",command=lambda:Boton(7)).grid(row=3,column=0)
+
+btn8=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="8",
+            bg="powder blue",command=lambda:Boton(8)).grid(row=3,column=1)
+
+btn9=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="9",
+            bg="powder blue",command=lambda:Boton(9)).grid(row=3,column=2)
+
+btn0=Button(root,padx=16,bd=8,fg="black",font=('arial',20,'bold'),text="0",
+            bg="powder blue",command=lambda:Boton(0)).grid(row=4,column=0)
 #---------operaciones basicas-------------------------------------------
 btnmenos=Button(root,padx=18,bd=8,fg="black",font=
             ('arial',20,'bold'),text="-",bg="powder blue").grid(row=1,column=3)
